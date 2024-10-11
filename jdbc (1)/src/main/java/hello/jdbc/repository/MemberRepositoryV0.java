0c6e8a127df1c2d48998a3fe1d1f0f0023e2e0f1
wooyeong <wooyeong@wooyeongui-MacBookPro.local>
2024-10-11 16:45:03 +0900

    Update pure JDBC

--- jdbc (1)/src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
+++ jdbc (1)/src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
@@ -1,4 +1,144 @@
 package hello.jdbc.repository;
 
+import hello.jdbc.connection.DBConnectionUtil;
+import hello.jdbc.domain.Member;
+import lombok.extern.slf4j.Slf4j;
+
+import java.sql.*;
+import java.util.NoSuchElementException;
+
+/***
+ * JDBC - DriverManager 사용
+ */
+
+@Slf4j
 public class MemberRepositoryV0 {
+
+    public Member save(Member member) throws SQLException {
+        String sql = "insert into member(member_id, money) values (?, ?)";
+
+        Connection con = null;
+        PreparedStatement pstmt = null;
+
+        try{
+            con = getConnection();
+            //pstmt ==> sql "?" 바인딩하는 값
+            pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, member.getMemberId());
+            pstmt.setInt(2, member.getMoney());
+            pstmt.executeUpdate();
+            return member;
+        }catch (SQLException e) {
+            log.error("db error", e);
+            throw e;
+        }finally{
+            //무조건 con 종료 해줘야함 (외부 리소스 사용중)
+            close(con, pstmt, null);
+        }
+
+    }
+
+    public Member findById(String memberId) throws SQLException {
+        String sql = "select * from member where member_id = ?";
+
+        Connection con = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
+        try{
+            con = getConnection();
+            pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, memberId);
+
+            //조회 결과
+            rs = pstmt.executeQuery();
+            if (rs.next()){ // 처음에는 아무것도 안가리키다, next()호출하면 첫번째부터 순차적으로
+                Member member = new Member();
+                member.setMemberId(rs.getString("member_id"));
+                member.setMoney(rs.getInt("money"));
+                return member;
+            }else{
+                throw new NoSuchElementException("member not found memberId="+memberId);
+            }
+        } catch (SQLException e) {
+            log.error("db error", e);
+            throw e;
+        }finally {
+            close(con,pstmt,rs);
+        }
+    }
+
+    public void update(String memberId, int money) throws SQLException {
+        String sql = "update member set money=? where member_id=?";
+
+        Connection con = null;
+        PreparedStatement pstmt = null;
+
+        try {
+            con = getConnection();
+            pstmt = con.prepareStatement(sql);
+            pstmt.setInt(1, money);
+            pstmt.setString(2, memberId);
+            int resultSize = pstmt.executeUpdate();
+            log.info("resultSize={}", resultSize);
+        } catch (SQLException e) {
+            log.info("error",e);
+            throw e;
+        }finally {
+            close(con, pstmt, null);
+        }
+
+    }
+
+
+    public void delete(String memberId) throws SQLException {
+        String sql = "delete from member where member_id=?";
+
+        Connection con = null;
+        PreparedStatement pstmt = null;
+
+        try{
+            con = getConnection();
+            pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, memberId);
+            pstmt.executeUpdate();
+        }catch (SQLException e){
+            log.info("error",e);
+            throw e;
+        }finally {
+            close(con, pstmt, null);
+        }
+    }
+
+    private void close(Connection con, Statement stmt, ResultSet rs){
+
+        if (rs != null){
+            try {
+                rs.close();
+            } catch (SQLException e) {
+                log.info("error",e);
+            }
+        }
+
+        if(stmt != null){
+            try {
+                stmt.close();
+            } catch (SQLException e) {
+                log.info("error", e);
+            }
+        }
+
+        if( con != null ){
+            try {
+                con.close();
+            } catch (SQLException e) {
+                log.info("error",e);
+            }
+        }
+
+    }
+
+    private Connection getConnection() {
+        return DBConnectionUtil.getConnection();
+    }
 }
