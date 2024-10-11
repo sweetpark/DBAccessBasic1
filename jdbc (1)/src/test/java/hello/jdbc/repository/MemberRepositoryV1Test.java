b51c741268f041cfdd622d08aedd04afe2c9ba0c
wooyeong <wooyeong@wooyeongui-MacBookPro.local>
2024-10-11 18:10:27 +0900

    Update Connection Pool (DriverManagerDataSource + Hikari )

--- jdbc (1)/src/test/java/hello/jdbc/repository/MemberRepositoryV1Test.java
+++ jdbc (1)/src/test/java/hello/jdbc/repository/MemberRepositoryV1Test.java
@@ -1,21 +1,38 @@
 package hello.jdbc.repository;
 
+import com.zaxxer.hikari.HikariDataSource;
 import hello.jdbc.domain.Member;
 import lombok.extern.slf4j.Slf4j;
-import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import java.sql.SQLException;
 import java.util.NoSuchElementException;
 
-import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
+import static hello.jdbc.connection.ConnectionConst.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @Slf4j
-class MemberRepositoryV0Test {
+class MemberRepositoryV1Test {
 
+    MemberRepositoryV1 repository;
 
-    MemberRepositoryV0 repository = new MemberRepositoryV0();
+    @BeforeEach
+    void beforeEach(){
+        //기본 DirverManager - 항상 새로운 커넥션을 획득
+        //DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
+
+        //커넥션 풀링
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(URL);
+        dataSource.setUsername(USERNAME);
+        dataSource.setPassword(PASSWORD);
+
+        repository = new MemberRepositoryV1(dataSource);
+    }
+
 
 
     @Test
